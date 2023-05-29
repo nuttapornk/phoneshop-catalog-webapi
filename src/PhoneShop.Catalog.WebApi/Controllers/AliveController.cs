d2e5b4005b8622fc304d3094ab5c5d11da76f686
nuttaporn kunsin <to_andata@hotmail.com>
2023-05-29 13:33:06 +0700

    update connection string

---
+++ src/PhoneShop.Catalog.WebApi/Controllers/AliveController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhoneShop.Catalog.WebApi.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AliveController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    public AliveController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            ConnectionStringsMongoDb = _configuration.GetConnectionString("MongoDb"),
+            EnvironmentName = System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")
+        });
+    }
+}
